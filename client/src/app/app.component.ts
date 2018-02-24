d19596dceb81837d445a10a95eaa3afb260d8e64
kaushik066 <masterkaushikrao@gmail.com>
2018-02-24 13:16:26 +0530

    connecting end to end and deploying the application

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>My First Angular App</h1>'
-  
+  template: '<h1>My First Angular App</h1>',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular app';
 }
--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 
-var index = require("./routes/index");
-var tasks = require("./routes/tasks");
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
 
 var port = 8123;
+
 var app = express();
 
-// View engine
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
-app.engine('html',require('ejs').renderFile);
+//View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
-// set static folder
-app.use(express.static(path.join(__dirname,'client')));
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'client')));
 
-// body-parser middleware
+// Body Parser MW
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/',index);
-app.use('/api',tasks);
+app.use('/', index);
+app.use('/api', tasks);
 
-app.listen(port,function(){
-	console.log('Server started at port '+port);
+app.listen(port, function(){
+    console.log('Server started on port '+port);
 });
